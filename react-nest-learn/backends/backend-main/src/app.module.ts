7d7026ed7a5cce839fe3fac683845c3c525ea7a7
chaitanya-dharmawat-qp <chaitanya.dharmawat@questionpro.com>
2025-09-03 14:58:14 +0530

    initiazling backends

--- react-nest-learn/backends/backend-main/src/app.module.ts
+++ react-nest-learn/backends/backend-main/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { resolve } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        resolve(__dirname, `./config/.env.${process.env.ENVIRONMENT}`),
+      ],
+      load: [loadAppConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
