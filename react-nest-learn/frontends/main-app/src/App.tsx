27fe935b9760d19531f2b76bafe34e9536eff857
chaitanya-dharmawat-qp <chaitanya.dharmawat@questionpro.com>
2025-09-08 11:38:22 +0530

    adding tests , depencies

--- react-nest-learn/frontends/main-app/src/App.tsx
+++ react-nest-learn/frontends/main-app/src/App.tsx
@@ -1,9 +1,45 @@
-import {WuPrimaryNavbar} from '@npm-questionpro/wick-ui-lib'
+import { WuPrimaryNavbar } from '@npm-questionpro/wick-ui-lib'
+import { useState } from 'react'
+import { API_BASE_URL } from './constants/appConstants'
+import type { IServerResponse } from './types/IServerResponse'
+import type { IUser } from './types/IUser'
+
+const fetchUser = async (): Promise<IServerResponse<IUser>> => {
+  return fetch(`${API_BASE_URL}user`, {
+    method: 'GET',
+    headers: {
+
+      'Content-Type': 'application/json',
+    },
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    return response.json() as Promise<IServerResponse<IUser>>
+  })
+}
 
 export function App() {
+  const [userData, setUserData] = useState<IUser>()
+  const [errorMessage, setErrorMessage] = useState<string>()
+  fetchUser()
+    .then(response => {
+      if (response.data) {
+        setUserData(response.data)
+      } else {
+        setErrorMessage('Error in fetching User Data' + response.message)
+        // throw Error("Error in fetching user Data Response Object:"+response)
+      }
+    })
+    .catch(e => {
+      setErrorMessage(e)
+    })
   return (
     <>
       <p>helm</p>
+      {errorMessage && (
+        <p style={{color: 'red'}}>Error Occurred : {errorMessage}</p>
+      )}
       <WuPrimaryNavbar
         Links={[
           <a key="home" href="#" className="active">
@@ -20,6 +56,7 @@ export function App() {
           </a>,
         ]}
       />
+      {userData && <h1>{`User Name: ${userData?.name}`}</h1>}
     </>
   )
 }
---
+++ react-nest-learn/frontends/main-app/src/providers/AppProvider.tsx
@@ -0,0 +1,7 @@
+// file path: src/providers/AppProviders.tsx
+import React from 'react'
+
+export const AppProviders: React.FC<React.PropsWithChildren> = ({children}) => {
+  // Create a client using ref to avoid re-creating it on every render
+  return <>{children}</>
+}
