cc93b8abc95da1a8100570749fc2c398abea5fb3
chaitanya-dharmawat-qp <chaitanya.dharmawat@questionpro.com>
2025-09-09 15:21:38 +0530

    updating packs

--- react-nest-learn/backends/backend-main/src/app.module.ts
+++ react-nest-learn/backends/backend-main/src/app.module.ts
@@ -5,11 +5,12 @@ import { resolve } from 'path';
 import { AllExceptionsFilter } from '@modules/errorLogger/AllExceptionFilter';
 import { ErrorLoggerModule } from '@modules/errorLogger/ErrorLoggerModule';
 import { TodoModule } from '@modules/todo/TodoModule';
-import { APP_FILTER } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { loadAppConfig } from './config/loadAppConfig';
-import { AllExceptionsFilter } from '@modules/errorLogger/AllExceptionFilter';
+import { DatabaseModule } from '@modules/database/DatabaseModule';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [
